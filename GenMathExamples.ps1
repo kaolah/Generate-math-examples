6241a8e1cb08e98b2f7bc6db67cf0ffe7831fc89
kaolah <35081118+kaolah@users.noreply.github.com>
2020-05-19 16:19:48 +0200

    Add files via upload

---
+++ GenMathExamples.ps1
@@ -0,0 +1,71 @@
+#dokolko if you want count up to 20 you need to enter 21
+$dokolko = 21
+#$pocet  how many examples you want to generate
+$pocet=90
+
+$priklady=@()
+$pomocna = 0
+$n=@()
+
+$m=1
+#examples are generated
+for ($i=0; $i -lt $dokolko; $i++) {
+ for ($j=0; $j -lt $dokolko; $j++) {
+        $v= $i+$j
+        if ($v -lt $dokolko) {
+        $priklady+="$i + $j ="
+        $priklady+="$j + $i =" 
+        if ($i -ge $j) {$priklady+="$i - $j ="}
+        else {$priklady+="$j - $i ="}
+        }}}
+$pocetkombinacii = $priklady.count
+
+    do {
+        $nah = Get-Random -Maximum $pocetkombinacii
+         foreach ($nn in $n) {
+             if ($nn -eq $nah) { $pomocna = 1}
+            }
+        if ($pomocna -eq 1) {
+                    $pomocna = 0
+                }           
+     else {
+            $n+= $nah
+            $m++
+        } }
+     
+ until ($m -gt $pocet)
+
+
+  
+
+#save examples to WORD doc
+$dok = $dokolko-1
+Write-Host " "
+Write-Host "************************************************" -ForegroundColor Yellow
+Write-Host "Generating $pocet examples up to $dok  from posible combination $pocetkombinacii" -ForegroundColor Yellow
+Write-Host "************************************************" -ForegroundColor Yellow
+Write-Host " "
+$word=new-object -ComObject "Word.Application"
+$Word.Visible = $True
+ 
+$doc = $word.documents.Add() 
+$myDoc = $word.Selection
+$myDoc.Font.Size = 24
+$myDoc.PageSetup.TextColumns.SetCount(3)
+
+foreach ($mm in $n) {
+    $text1 = $priklady[$mm]
+    $myDoc.TypeText("$text1")
+    $myDoc.TypeParagraph()
+    
+    }
+
+Write-Host " "
+Write-host "*********** Done ****************" -ForegroundColor Red
+Write-Host " "
+ pause 
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($doc) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($word) | Out-Null
+Remove-Variable doc,Word
+[gc]::collect()
+[gc]::WaitForPendingFinalizers()#>
